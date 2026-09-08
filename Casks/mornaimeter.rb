881adc3623387bf57d6428591f316c0464f517db
matsufriends <pikapikan0526@gmail.com>
2026-09-08 14:30:11 +0900

    tap を廃止し tsukumistudio/tap へ誘導

--- Casks/mornaimeter.rb
+++ Casks/mornaimeter.rb
@@ -2,22 +2,14 @@ cask "mornaimeter" do
   version "0.6.0"
   sha256 "3820576c691823dda3a498d68d79fff895e7eee7eb8963eb48cf7bd8781b3a15"
 
-  url "https://github.com/matsufriends/MornAIMeter/releases/download/v#{version}/MornAIMeter.app.zip"
+  url "https://github.com/TsukumiStudio/MornAIMeter/releases/download/v#{version}/MornAIMeter.app.zip"
   name "MornAIMeter"
   desc "Menu bar pie charts for Claude Code / Codex CLI usage limits"
-  homepage "https://github.com/matsufriends/MornAIMeter"
+  homepage "https://github.com/TsukumiStudio/MornAIMeter"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  disable! date: "2026-09-08", because: "moved to tsukumistudio/tap. Run: brew untap matsufriends/tap && brew install --cask tsukumistudio/tap/mornaimeter"
 
   depends_on :macos
 
   app "MornAIMeter.app"
-
-  zap trash: [
-    "~/Library/Application Support/MornAIMeter",
-    "~/Library/Preferences/studio.tsukumi.mornaimeter.plist",
-  ]
 end
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# homebrew-tap
+# homebrew-tap (廃止)
+
+この tap は廃止しました。すべての cask は [tsukumistudio/tap](https://github.com/TsukumiStudio/homebrew-tap) に移管済みです。
 
 ```sh
-brew tap matsufriends/tap
+brew untap matsufriends/tap
+brew tap tsukumistudio/tap
+brew install --cask tsukumistudio/tap/mornaimeter
 ```
